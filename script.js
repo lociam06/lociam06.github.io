57673c5fb0d187e6badda1ea71b45e05ad9b3629
lociam06 <betances2607@gmail.com>
2024-07-08 14:40:29 -0400

    Puse los archivos

---
+++ script.js
@@ -0,0 +1,115 @@
+//variables goblales del tiempo
+let hours = 0, minutes = 0, seconds = 0;
+
+//Referencia del tiempo principal
+var principalTimeNode = document.querySelector("#time");
+
+//Referencia al modo de time
+var timeMode = document.querySelector("#time_mode")
+
+//Funcion que se ejecuta cada segundo
+var countEverySeconds;
+
+//Referencia de los temporizadores
+var focusTimeInputs = document.querySelectorAll(".focus_timer input");
+var restTimeInputs = document.querySelectorAll(".rest_timer input");
+
+var allTimeImputs = document.querySelectorAll(".timers_continer input");
+
+//convirtiendo las listas de inputs a arrays (para que pueda ser manejado con el worker)
+var focusTimeValues = Array.from(focusTimeInputs).map(input => input.value);
+var restTimeValues = Array.from(restTimeInputs).map(input => input.value);
+
+//Referencia de los botones
+var iniciarBtn = document.querySelector(".buttons #iniciar_btn");
+var pausarBtn = document.querySelector(".buttons #pausar_btn");
+var reiniciarBtn = document.querySelector(".buttons #reiniciar_btn");
+var cambiarBtn = document.querySelector(".buttons #cambiar_btn");
+
+//Eventlistener de los botones
+iniciarBtn.addEventListener("click", iniciarBtnClick);
+pausarBtn.addEventListener("click", pausarBtnClick);
+reiniciarBtn.addEventListener("click", reiniciarBtnClick);
+cambiarBtn.addEventListener("click", cambiarBtnClick);
+
+//Detecta la consistencia de los valores el valor
+for(let i = 0; i < allTimeImputs.length; i++){
+    let input = allTimeImputs[i];
+    input.addEventListener('change', function(){
+        if(input.value > parseInt(input.max)) {
+            input.value = parseInt(input.max);
+        }
+        else if(input.value < parseInt(input.min)) input.value = parseInt(input.min);
+        
+        if(input.value == "") input.value = 0;
+        focusTimeValues = Array.from(focusTimeInputs).map(input => input.value);
+        restTimeValues = Array.from(restTimeInputs).map(input => input.value);
+        timeWorker.postMessage({command: "reasing time", focusTimeValues: focusTimeValues, restTimeValues: restTimeValues});
+    })
+
+//Auto seleccionar
+    input.addEventListener('click', function(e) {
+        let input2 = e.target;
+        input2.select();
+    })
+}
+
+//Modifica el contador del DOM
+function modifiePrincipalTime(){
+    principalTimeNode.textContent = "" + (hours > 9? hours : "0" + hours) + ":" + (minutes > 9? minutes : "0" + minutes) + ":" + (seconds > 9? seconds : "0" + seconds); 
+}
+//Modifica el titulo de la pagina
+function modifiePageTittle(){
+    document.title =  "" + (hours > 9? hours : "0" + hours) + ":" + (minutes > 9? minutes : "0" + minutes) + ":" + (seconds > 9? seconds : "0" + seconds) + " Pomodoro timer";
+}
+function CambiarTimeModeTittle(text, color){
+    timeMode.textContent = text;
+    timeMode.style.color = color;
+}
+
+//reasignar valores
+function reasingArrayTimerValues(){
+    focusTimeValues = Array.from(focusTimeInputs).map(input => input.value);
+    restTimeValues = Array.from(restTimeInputs).map(input => input.value);
+}
+
+//Sonido de alarma
+var alarmSong = new Audio();
+alarmSong.src = "./sound/Minecraft\ Level\ Up\ Sound\ Effect.mp3"
+
+//Worker
+var timeWorker = new Worker('./worker.js');
+timeWorker.postMessage({command: "start program", focusTimeValues: focusTimeValues, restTimeValues: restTimeValues});
+
+//Funcione a la que llaman los botones
+function iniciarBtnClick(){
+    reasingArrayTimerValues();
+    timeWorker.postMessage({command: "start"})
+}
+function pausarBtnClick(){timeWorker.postMessage({command: "pause"})}
+function reiniciarBtnClick(){timeWorker.postMessage({command: "restart"})}
+function cambiarBtnClick(){timeWorker.postMessage({command: "change"})}
+
+timeWorker.onmessage = function(e){
+    let data = e.data;
+    if(data.command == "update"){
+        hours = e.data.hours;
+        minutes = e.data.minutes;
+        seconds = e.data.seconds;
+        modifiePrincipalTime();
+        modifiePageTittle();
+    }
+
+    if(data.command == "chage time mode tittle"){
+        CambiarTimeModeTittle(data.text, data.color);
+    }
+    if(data.command == "play song"){
+        alarmSong.play();
+    }
+
+    if(data.command == "reasing time"){
+        focusTimeValues = Array.from(focusTimeInputs).map(input => input.value);
+        restTimeValues = Array.from(restTimeInputs).map(input => input.value);
+        this.postMessage({command: "reasing time", focusTimeValues: focusTimeValues, restTimeValues: restTimeValues});
+    }
+}
---
+++ worker.js
@@ -0,0 +1,186 @@
+//variables goblales del tiempo
+let hours = 0, minutes = 0, seconds = 0;
+
+//Clase de Temporizadores
+class Timer{
+    constructor(asociatedTime){
+        this.asociatedTime = asociatedTime;
+    }
+
+    reasing(){
+        this.hours = parseInt(this.asociatedTime[0]);
+        this.minutes = parseInt(this.asociatedTime[1]);
+        this.seconds = parseInt(this.asociatedTime[2]);
+    }
+}
+//temporizadores
+var focusTime;
+var restTime;
+
+//Temporizador en uso
+var mainTimer;
+var mainTimerID = 0;
+
+//Verifica si el temporizador se ha iniciado
+let reseted = true;
+
+//Verifica el estado del timer
+let stopped = true;
+
+//Intervalo
+let countEverySeconds;
+
+//Cuenta cuando pasa cada segundo
+function CountTime(){
+    seconds--;
+    if(hours == 0 && minutes == 0 && seconds <= 0) {
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        RestartGlobalTime();
+        CambiarTimer();
+        postMessage({command: "play song"});
+        reseted = true;
+    }
+    if(seconds < 0) {
+        minutes--;
+        seconds += 60;
+    }
+    if(minutes < 0) {
+        hours--;
+        minutes += 60;
+    }
+    UpdateDOM();
+}
+
+//---Funciones del tiempo----
+function IniciarTimer(){
+    if(reseted == true){
+        focusTime.reasing();
+        restTime.reasing();
+    
+        RestartGlobalTime();
+        UpdateDOM();
+        
+        reseted = false;
+        stopped = false;
+    }
+    if(countEverySeconds == null) {
+        countEverySeconds = setInterval(CountTime, 1000);
+    }
+    else{
+        clearInterval(countEverySeconds);
+        countEverySeconds = setInterval(CountTime, 1000)
+    }
+}
+
+function PausarTimer(){
+    if(countEverySeconds != null) {
+        clearInterval(countEverySeconds);
+        stopped = true;
+    }
+}
+
+function ReiniciarTimer(autoStop){
+    focusTime.reasing();
+    restTime.reasing();
+
+    RestartGlobalTime();
+
+    if(autoStop) PausarTimer();
+    
+    postMessage({command: "reasing time"});
+
+    UpdateDOM();
+    
+    reseted = true;
+}
+
+function CambiarTimer(){
+    CambiarTimeModeTittle();
+
+    if(mainTimerID == 0) {
+        mainTimer = restTime;
+        mainTimerID = 1;
+    }
+    else if(mainTimerID == 1) {
+        mainTimer = focusTime;
+        mainTimerID = 0;
+    }
+
+    RestartGlobalTime();
+    UpdateDOM();
+    ReiniciarTimer(false);
+}
+
+function CambiarTimeModeTittle(){
+    let text;
+    let color;
+    if(mainTimerID == 0) {
+        text = "Descanso"
+        color = "#ff0"
+    }
+    else if(mainTimerID == 1) {
+        text = "Consentración"
+        color = "#36f"
+    }
+
+    postMessage({command: "chage time mode tittle", text: text, color: color})
+}
+
+function RestartGlobalTime(){
+    hours = mainTimer.hours;
+    minutes = mainTimer.minutes;
+    seconds = mainTimer.seconds;
+    UpdateDOM();
+}
+
+//Actualiza los datos del dom
+function UpdateDOM(){
+    postMessage({command: "update", hours: hours, minutes: minutes, seconds: seconds});
+}
+
+//Llamadas del worker
+onmessage = function(e){
+    //al iniciar el programa
+    if(e.data.command == "start program"){
+        //Declarando remporizadores
+        focusTime = new Timer(e.data.focusTimeValues)
+        focusTime.reasing();
+        console.log(focusTime)
+
+        restTime = new Timer(e.data.restTimeValues)
+        restTime.reasing();
+
+        mainTimer = focusTime;
+    }
+
+    if(e.data.command == "start"){
+        IniciarTimer();
+    }
+
+    if(e.data.command == "pause"){
+        PausarTimer();
+    }
+
+    if(e.data.command == "restart"){
+        ReiniciarTimer();
+    }
+
+    if(e.data.command == "change"){
+        CambiarTimer();
+    }
+
+    if(e.data.command == "reasing time"){
+        if(reseted) {
+            focusTime.asociatedTime = e.data.focusTimeValues;
+            restTime.asociatedTime = e.data.restTimeValues;
+            focusTime.reasing();
+            restTime.reasing();
+
+            if(mainTimerID == 0) mainTimer = focusTime;
+            else if(mainTimerID == 1) mainTimer = restTime;
+            RestartGlobalTime();
+        }
+    }
+}
